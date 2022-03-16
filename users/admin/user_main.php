67e82ba8fd86637c7715e20fd6b0fbe280aff79c
Drendar Josh Dimasacat <drendarjosh@gmail.com>
2022-03-16 13:31:23 +0800

    initial

---
+++ users/admin/user_main.php
@@ -0,0 +1,24 @@
+<?php
+
+
+session_start();
+if(isset($_SESSION['Uname'])){
+    
+   }
+   else{
+ 
+   
+}
+include('security.php');
+
+include('includes/header.php');
+include('includes/navbar.php');
+?>
+
+
+
+
+<?php
+include('includes/scripts.php');
+include('includes/footer.php');
+?>
